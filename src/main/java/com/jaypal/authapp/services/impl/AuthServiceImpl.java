ec7d89f54551c471ce3181c0268784e3944ef15b
Jaypal Koli <kolijaypal47@gmail.com>
2025-12-02 17:22:32 +0530

    Implement register logic

--- src/main/java/com/jaypal/authapp/services/impl/AuthServiceImpl.java
+++ src/main/java/com/jaypal/authapp/services/impl/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.jaypal.authapp.dtos.UserDto;
 import com.jaypal.authapp.services.AuthService;
 import com.jaypal.authapp.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDto registerUser(UserDto userDto) {
         //logic
         //verify email
         //default role
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         return userService.createUser(userDto);
     }
 }
